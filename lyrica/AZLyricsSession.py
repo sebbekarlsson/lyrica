7ace3d74a7becb40cc3dff3786ea4d1f0f6c9e9a
Sebastian karlsson <sebbekarlsson97@gmail.com>
2018-04-15 12:40:47 +0200

    added another source, azlyrics, closes #1

---
+++ lyrica/AZLyricsSession.py
@@ -0,0 +1,29 @@
+from requests import Session
+from bs4 import BeautifulSoup
+
+
+class AZLyricsSession(object):
+
+    def __init__(self):
+        self.base_url = 'https://www.azlyrics.com/lyrics'
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
+        }
+        self.session = Session()
+
+    def get_lyrics(self, artist, song):
+        url = self.base_url + '/{}/{}.html'.format(
+            artist.replace(' ', ''),
+            song.replace(' ', '')
+        )
+
+        resp = self.session.get(url, headers=self.headers)
+
+        soup = BeautifulSoup(resp.text, 'html.parser')
+        element = soup.select_one('''
+            body > div.container.main-page >
+            div > div.col-xs-12.col-lg-8.text-center >
+            div:nth-of-type(5)
+        ''')
+
+        return element.text if element else None
--- lyrica/Lyrica.py
+++ lyrica/Lyrica.py
@@ -1,10 +1,14 @@
 from lyrica.GeniusSession import GeniusSession
+from lyrica.AZLyricsSession import AZLyricsSession
 
 
 class Lyrica(object):
 
     def __init__(self):
-        self.sessions = [GeniusSession()]
+        self.sessions = [
+            GeniusSession(),
+            AZLyricsSession()
+        ]
 
     def get_lyrics(self, artist, song):
         for session in self.sessions:
