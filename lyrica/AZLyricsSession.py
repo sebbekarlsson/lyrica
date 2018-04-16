34b1db5212eaa26f8f7741e41110075405924b0f
Sebastian karlsson <sebbekarlsson97@gmail.com>
2018-04-16 12:57:50 +0200

    user agent column 80

--- lyrica/AZLyricsSession.py
+++ lyrica/AZLyricsSession.py
@@ -9,7 +9,9 @@ class AZLyricsSession(SiteSession):
         SiteSession.__init__(self)
         self.base_url = 'https://www.azlyrics.com/lyrics'
         self.headers = {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3)\
+            AppleWebKit/537.36 (KHTML, like Gecko)\
+            Chrome/65.0.3325.181 Safari/537.36'
         }
 
     def get_lyrics(self, artist, song):
