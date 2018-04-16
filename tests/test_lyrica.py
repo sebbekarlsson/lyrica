1c6678e917f058e61d5d826acfd32ff3a7713049
Sebastian karlsson <sebbekarlsson97@gmail.com>
2018-04-16 11:10:59 +0200

    added tests for lyrica class

---
+++ tests/test_lyrica.py
@@ -0,0 +1,22 @@
+from lyrica.Lyrica import Lyrica
+
+
+lyrica = Lyrica()
+
+
+def test_no_lyrics():
+    lyrics = lyrica.get_lyrics('Artist Does not Exist', 'No such Song')
+
+    assert lyrics is None
+
+
+def test_lyrics():
+    lyrics = lyrica.get_lyrics('tom waits', 'hold on')
+
+    assert type(lyrics) is unicode
+    assert 'sign' in lyrics
+
+    lyrics = lyrica.get_lyrics('pink floyd', 'shine on you crazy diamond')
+
+    assert type(lyrics) is unicode
+    assert 'piper' in lyrics
